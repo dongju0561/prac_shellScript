30c4f615c5927d383cdff7acfca84fe9c1376096
dongju park <pdj95044@gmail.com>
2024-06-12 18:07:07 +0900

    입력 받은 숫자 if문으로 홀수 혹은 짝수 확인

--- oddEven.sh
+++ oddEven.sh
@@ -3,6 +3,24 @@
 echo "Enter a number: "
 read num
 
+# 띄어쓰기 주의
+
+# if [ 조건 ]
+# then
+#     실행문
+# else
+#     실행문
+# fi
+
+# 산술 연산자
+# -eq : equal
+# -ne : not equal
+# -gt : greater than
+# -lt : less than
+# -ge : greater than or equal
+# -le : less than or equal
+
+
 if [ $((num % 2)) -eq 0 ]
 then
     echo "This number is even"
